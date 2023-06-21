634b507b03c618fa1dfd88014572e917e2676a00
DrCheeseFace <tharun1@hotmail.co.uk>
2023-06-21 19:09:00 +0100

    initial commit

---
+++ mysite/polls/views.py
@@ -0,0 +1,49 @@
+from django.http import HttpResponse, HttpResponseRedirect 
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.urls import reverse
+from django.views import generic 
+from django.db import models
+from .models import Question, Choice
+from datetime import datetime 
+from django import forms
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5] 
+    
+class DetailView(generic.DetailView):
+    model = Question 
+    template_name = "polls/detail.html"
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html",
+                      {"question": question, 
+                       "error_message": "You didn't select a choice", 
+                       },
+                       )
+    else: 
+        selected_choice.votes += 1
+        selected_choice.save()
+    
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id, )))
+
+def addQuestion(request):
+    if request.method == "POST":
+        form = Question()
+        form.question_text = request.POST.get('question_text')
+        form.pub_date = str(timezone.now())
+        form.save()
+        return HttpResponseRedirect(reverse("polls:index"))
+    return render(request, 'polls/addquestion.html')
